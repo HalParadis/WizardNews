71991d3d4d5e2401579bed234710c1fc4280f8e1
HalParadis <halparadis@gmail.com>
2023-05-03 21:02:48 -0700

    initial commit: list of titles and authors

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const {list, find} = require("./postBank");
+const morgan = require("morgan");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+const posts = list();
+
+// app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get('/posts', (req, res) => {
+  res.send(`<ul>${posts.map(post => `<li>${post.title} by ${post.name}</li>`).join('')}</ul>`);
+});
+
 
 const PORT = 1337;
 
